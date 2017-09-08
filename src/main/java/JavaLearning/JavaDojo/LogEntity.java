361a81242a90419d0e9c146018afc97183a8f8d2
xfzeng <2590542465@qq.com>
2017-09-08 22:58:53 +0800

    本次修改初次创建了一个LogEntity的实体类，定义了相关变量及其set get方法

---
+++ src/main/java/JavaLearning/JavaDojo/LogEntity.java
@@ -0,0 +1,85 @@
+package JavaLearning.JavaDojo;
+
+public class LogEntity {
+
+	private String ip;
+	private String requestTime;
+	private String requestMethod;
+	private String requestPath;
+	private String requestVersion;
+	private String requestStatus;
+	private String requestFromUrl;
+	private String terminalName;
+	private String terminalVersion;
+	private String terminalDescription;
+	private String updateTime;
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getRequestTime() {
+		return requestTime;
+	}
+	public void setRequestTime(String requestTime) {
+		this.requestTime = requestTime;
+	}
+	public String getRequestMethod() {
+		return requestMethod;
+	}
+	public void setRequestMethod(String requestMethod) {
+		this.requestMethod = requestMethod;
+	}
+	public String getRequestPath() {
+		return requestPath;
+	}
+	public void setRequestPath(String requestPath) {
+		this.requestPath = requestPath;
+	}
+	public String getRequestVersion() {
+		return requestVersion;
+	}
+	public void setRequestVersion(String requestVersion) {
+		this.requestVersion = requestVersion;
+	}
+	public String getRequestStatus() {
+		return requestStatus;
+	}
+	public void setRequestStatus(String requestStatus) {
+		this.requestStatus = requestStatus;
+	}
+	public String getRequestFromUrl() {
+		return requestFromUrl;
+	}
+	public void setRequestFromUrl(String requestFromUrl) {
+		this.requestFromUrl = requestFromUrl;
+	}
+	public String getTerminalName() {
+		return terminalName;
+	}
+	public void setTerminalName(String terminalName) {
+		this.terminalName = terminalName;
+	}
+	public String getTerminalVersion() {
+		return terminalVersion;
+	}
+	public void setTerminalVersion(String terminalVersion) {
+		this.terminalVersion = terminalVersion;
+	}
+	public String getTerminalDescription() {
+		return terminalDescription;
+	}
+	public void setTerminalDescription(String terminalDescription) {
+		this.terminalDescription = terminalDescription;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+	
+
+
+}
