515c8144a9659f11fa855ced661f2419a39156d3
xfzeng <2590542465@qq.com>
2017-09-08 16:19:42 +0800

    以空格分割字符串为几段子字符串数组，再用循环依次倒序拼接，完成句子中只改变单词字母顺序而不改变单词位置顺序的要求

--- src/main/java/JavaLearning/JavaDojo/App.java
+++ src/main/java/JavaLearning/JavaDojo/App.java
@@ -12,14 +12,37 @@ public class App
     	{
     		return "";
     	}
-    	String str1=str.replaceAll(" ", "");
-    	return new StringBuffer(str1).reverse().toString();
-    	
+    	//String str1=str.replaceAll(" ", "");
+    	//return new StringBuffer(str).reverse().append(str).toString();
+    	//以空格分割字符串为几段子字符串数组
+    	String[] s = str.split(" ");
+    	//创建一个字符串变量
+    	  String s1 = "";
+    	//以数组元素个数为限制进行循环
+    	  for (int i = 0; i < s.length; i++) 
+    	  {
+    		//将子数组倒序  
+    	   String s2 = new StringBuffer(s[i]).reverse().toString();
+    	   //将倒序后的数组放入s1中拼接
+    	   if ("".equals(s1))
+    	    {
+    		   s1 = s2;
+    	    }
+    	   else
+    	    {
+    		   s1=s1+" "+s2;
+    	    }
+    	  }
+    	  return s1;
+
+         
+        
     	
     }
 	public static void main( String[] args )
     {
-		
-        System.out.println( "Hello World!" );
+		App a = new App();
+		a.output("hello world");
+        //System.out.println( "Hello World!" );
     }
 }
