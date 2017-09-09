9a91ee81bc6adc36c726c811e2fa44646fe35365
xfzeng <2590542465@qq.com>
2017-09-09 23:51:21 +0800

    本次修改实现了两个方法的重构，并改善了命名，提高了可读性

--- src/main/java/JavaLearning/JavaDojo/LogHandler.java
+++ src/main/java/JavaLearning/JavaDojo/LogHandler.java
@@ -1,7 +1,7 @@
 package JavaLearning.JavaDojo;
 
 import java.text.*;
-import java.util.Locale;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,20 +9,33 @@ public class LogHandler {
 
 	public LogEntity getEntity(String logStr) {
 		LogEntity logEntity = new LogEntity();
-		String ipRegex = "\\d+\\.\\d+\\.\\d+\\.\\d+";
-		Matcher ipMatcher = Pattern.compile(ipRegex).matcher(logStr);
-		ipMatcher.find();
-		logEntity.setIp(ipMatcher.group(0));
+		getIpFromString(logStr, logEntity);
+		logEntity.setIp(getIpFromString(logStr, logEntity));
+		getTimeFromString(logStr, logEntity);
+		logEntity.setRequestTime(getTimeFromString(logStr, logEntity));
+		return logEntity;
+	}
+
+	private Date getTimeFromString(String logStr, LogEntity logEntity) {
+		Date date = null;
 		String timeRegex = "\\d+\\/\\w+\\/\\d+\\:\\d+\\:\\d+\\:\\d+\\s\\+\\d+";
 		Matcher TimeMatcher = Pattern.compile(timeRegex).matcher(logStr);
 		TimeMatcher.find();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH);
 		try {
-			logEntity.setRequestTime(dateFormat.parse(TimeMatcher.group(0)));
+			date = dateFormat.parse(TimeMatcher.group(0));
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		return logEntity;
+		return date;
+
+	}
+
+	private String getIpFromString(String logStr, LogEntity logEntity) {
+		String ipRegex = "\\d+\\.\\d+\\.\\d+\\.\\d+";
+		Matcher ipMatcher = Pattern.compile(ipRegex).matcher(logStr);
+		ipMatcher.find();
+		return ipMatcher.group(0);
 	}
 
 }
