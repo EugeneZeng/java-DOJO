4b70542e00634937c03e623ab3a113416ff4bb1b
Eugene Zeng <euene.zeng@live.com>
2017-09-10 00:11:51 +0800

    Added three more test cases.

--- src/test/java/JavaLearning/JavaDojo/LogHandlerTest.java
+++ src/test/java/JavaLearning/JavaDojo/LogHandlerTest.java
@@ -1,5 +1,9 @@
 package JavaLearning.JavaDojo;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import junit.framework.TestCase;
 
 public class LogHandlerTest extends TestCase {
@@ -13,6 +17,23 @@ public class LogHandlerTest extends TestCase {
 		String logStr = "183.193.187.122 - - [24/Dec/2016:00:13:14 +0800] \"POST /socket.io/?EIO=3&transport=polling&j=12&t=LaiTH9P&b64=1&sid=rWxYshIVfGqzC15yAAEV HTTP/1.1\" 200 2 \"http://pmchat.24k.hk/studio?utm_source=pn14&utm_medium=ag&utm_campaign=yy&utm_content=AD_pcuiZS_top\" \"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)\"";
 		LogEntity logEntity = logHandler.getEntity(logStr);
 	    assertEquals(logEntity.getIp(), "183.193.187.122");
+	    
+	    Date reqTime = logEntity.getRequestTime();
+	    Calendar cal = Calendar.getInstance();
+	    cal.setTime(reqTime);
+	    assertEquals(cal.get(Calendar.DATE), 24);
+	    assertEquals(cal.get(Calendar.MONTH), 11);
+	    assertEquals(cal.get(Calendar.YEAR), 2016);
+	    assertEquals(cal.get(Calendar.HOUR_OF_DAY), 0);
+	    assertEquals(cal.get(Calendar.MINUTE), 13);
+	    assertEquals(cal.get(Calendar.SECOND), 14);
+	    assertEquals(cal.get(Calendar.ZONE_OFFSET), 8 * 3600 * 1000);
+	    
+	    String method = logEntity.getRequestMethod();
+	    assertEquals(method, "POST");
+	    
+	    String requestPath = logEntity.getRequestPath();
+	    assertEquals(requestPath, "/socket.io/?EIO=3&transport=polling&j=12&t=LaiTH9P&b64=1&sid=rWxYshIVfGqzC15yAAEV");
 	}
 
 }
