352efb4d6b8bbd6e5ae2cfa9c35d709b247a44a4
xfzeng <2590542465@qq.com>
2017-09-09 22:43:24 +0800

    本次修改提高了代码的简洁度，提高了可读性

--- src/main/java/JavaLearning/JavaDojo/LogHandler.java
+++ src/main/java/JavaLearning/JavaDojo/LogHandler.java
@@ -8,34 +8,20 @@ import java.util.regex.Pattern;
 public class LogHandler {
 
 	public LogEntity getEntity(String logStr) {
-		// 创建一个LogEntity对象
 		LogEntity logEntity = new LogEntity();
-		// 建立IP地址的正则表达式过滤规则
 		String ipRegex = "\\d+\\.\\d+\\.\\d+\\.\\d+";
-		// 创建pattern对象和matcher对象，进行编译和匹配
-		// Pattern pattern = Pattern.compile(ipRegex);
 		Matcher ipMatcher = Pattern.compile(ipRegex).matcher(logStr);
 		ipMatcher.find();
-		// 把匹配结果放入LogEntity类中的变量ip中
 		logEntity.setIp(ipMatcher.group(0));
-		System.out.println(logEntity.getIp());
-		// 建立时间的正则表达式规则
-		String timeRegex = "\\d{2}\\/[A-Z]{1}[a-z]{2}\\/\\d{4}\\:\\d+\\:\\d+\\:\\d+\\s\\+\\d+";
+		String timeRegex = "\\d+\\/\\w+\\/\\d+\\:\\d+\\:\\d+\\:\\d+\\s\\+\\d+";
 		Matcher TimeMatcher = Pattern.compile(timeRegex).matcher(logStr);
 		TimeMatcher.find();
-		// 把匹配结果放入LogEntity类中的变量中
-		// logEntity.setRequestTime(TimeMatcher.group(0));
-		System.out.println(TimeMatcher.group(0));
-		// 格式转换
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH);
-		// 转换完成后赋值给LogEntity类中的变量RequestTime中
 		try {
 			logEntity.setRequestTime(dateFormat.parse(TimeMatcher.group(0)));
-			System.out.println(logEntity.getRequestTime());
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-
 		return logEntity;
 	}
 
