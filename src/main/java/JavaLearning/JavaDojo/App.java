9f3dfce64cba85f1b859c7d8ff118386f5a050a6
xfzeng <2590542465@qq.com>
2017-09-14 21:40:09 +0800

    本次修改实现了信息添加入数据库的功能

--- src/main/java/JavaLearning/JavaDojo/App.java
+++ src/main/java/JavaLearning/JavaDojo/App.java
@@ -1,5 +1,10 @@
 package JavaLearning.JavaDojo;
 
+import java.sql.*;
+import java.util.*;
+
+import org.apache.commons.dbutils.*;
+
 //import java.lang.*;
 /**
  * Hello world!
@@ -29,13 +34,40 @@ public class App {
 		return result;
 	}
 
-	private void insertLogEntity(LogEntity logEntity) {
-		// TODO Auto-generated method stub
-		
+	
+	
+	private Connection getDBConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:e:/workspace/java-dojo/SQLite/dojo.db");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
 	}
 
-	public static void main(String[] args) {
-		App app = new App();
+	private void insertLogEntity(LogEntity logEntity) throws SQLException {
+		QueryRunner run = new QueryRunner();
+		String sql = "INSERT INTO LogEntities(ip,requestTime,requestMethod,requestPath,requestVersion,requestStatus,requestFromUrl,terminalInfor) VALUES(?,?,?,?,?,?,?,?)";
+		Object[] params = { logEntity.getIp(), logEntity.getRequestTime(), logEntity.getRequestMethod(),
+				logEntity.getRequestPath(), logEntity.getRequestVersion(), logEntity.getRequestStatus(),
+				logEntity.getRequestFromUrl(), logEntity.getTerminalInfor() };
+		Connection connection = getDBConnection();
+
+		try {
+			 run.update(connection, sql, params);
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+		DbUtils.closeQuietly(connection);
+	
+	}
+
+	public static void main(String[] args) throws SQLException {
+		App app = new App();		
 		LogHandler logHandler = new LogHandler();
 		String logStr = "183.193.187.122 - - [24/Dec/2016:00:13:14 +0800] \"POST /socket.io/?EIO=3&transport=polling&j=12&t=LaiTH9P&b64=1&sid=rWxYshIVfGqzC15yAAEV HTTP/1.1\" 200 2 \"http://pmchat.24k.hk/studio?utm_source=pn14&utm_medium=ag&utm_campaign=yy&utm_content=AD_pcuiZS_top\" \"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)\"";
 		LogEntity logEntity = logHandler.getEntity(logStr);
